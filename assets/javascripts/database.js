af308df73779d1419f37b720f6cf333e65930185
294797392@qq.com <294797392@qq.com>
2018-03-10 11:17:48 +0800

    .

---
+++ assets/javascripts/database.js
@@ -0,0 +1,20 @@
+﻿
+
+/// <reference path="jquery.js"/>
+
+
+var database_url = "http://localhost:8888/";
+
+var database = {
+    get_menu_list: function (callback) {
+        $.ajax({
+            url: database_url + "svc/menu.json",
+            success: function (data, textStatus, jqXHR) {
+                if (callback) {
+                    //var menus = JSON.parse(data);
+                    callback(data);
+                }
+            }
+        });
+    }
+};
--- assets/javascripts/vms/app.js
+++ assets/javascripts/vms/app.js
@@ -11,28 +11,42 @@
 
 
 /// <reference path="../knockout.js"/>
+/// <reference path="../database.js"/>
 
 
 
 
 
 
-var cls_app_content_vm = function () {
-    this.content = ko.observable();
-};
-
-var cls_app_menu_item_vm = function () {
+var app_menu_item_vm = function () {
     this.title = ko.observable();
     this.icon = ko.observable();
     this.uri = ko.observable();
 };
 
-var cls_app_main_menu_vm = function () {
+var app_main_menu_vm = function () {
     this.menus = ko.observableArray();
 };
 
-var func_app_init = function () {
+var app_init = function () {
 
+    
 
+    // 定义变量
+    var menu_item_vm = new app_main_menu_vm();
+    var callback_get_menu_list = function (menus) {
+        var len = menus.length;
+        for (var i = 0; i < len; i++) {
+            var menu = menus[i];
+            var menu_item = new app_menu_item_vm();
+            menu_item.title(menu.title);
+            menu_item_vm.menus.push(menu_item);
+        }
+    }
 
+    // 绑定ViewModel
+    ko.applyBindings({ menus: menu_item_vm.menus });
+
+    // 获取读取
+    database.get_menu_list(callback_get_menu_list);
 };
